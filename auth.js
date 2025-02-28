63cdc7f7da8c2c5e3a9ff00f1efa4c46e60c4c81
ASF <vehicledevelopment@asf-ev.com>
2025-02-28 09:37:50 +0900

    パスワード変更

--- auth.js
+++ auth.js
@@ -1,13 +1,13 @@
 const P = btoa("ASF-AfterService");
 
 function PPP() {
-    const PP = document.getElementById("ASF-AfterService").value;
-    const EP = btoa(PP);
+    const PPP = document.getElementById("ASF-AfterService").value;
+    const EP = btoa(PPP);
 
     if (EP === P) {
         sessionStorage.setItem("loggedIn", "true");
         window.location.href = "links.html";
     } else {
-        alert("パスワードが違います");
+        alert("パスワードが違います"); 
     }
 }
